37dc437719680f5f6b46b0ac346895c459a6b58a
nahye55 <nahye55@naver.com>
2020-03-20 19:12:08 +0900

    200320 commit - reduce() 메소드 사용

---
+++ programmers/level1/평균 구하기.js
@@ -0,0 +1,34 @@
+/*
+  문제 설명
+  정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+
+  제한사항
+  arr은 길이 1 이상, 100 이하인 배열입니다.
+  arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+  입출력 예
+  arr	return
+  [1,2,3,4]	2.5
+  [5,5]	5
+ */
+
+function solution(arr) {
+
+  let sum = 0;
+  let length = arr.length;
+  for(let i=0; i<length; i++) {
+    sum += arr[i];
+  }
+
+  return sum/length;
+
+}
+
+function solution2(arr) {
+  return arr.reduce((a,b) => a+b) / arr.length;
+}
+
+/*
+ * 개선 사항
+ * solution2() : reduce 문법 사용
+ * 
+ */
